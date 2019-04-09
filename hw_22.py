d82d8b4cbb90acdd7f8699423ab012a670e05743
SoomMov <sum_mov@yahoo.com>
2019-04-09 23:19:56 +0400

    committed

---
+++ hw_22.py
@@ -0,0 +1,13 @@
+a = input("input a: ")
+b = input("input b: ")
+c = input("input c: ")
+
+if (c < b):
+    if (c < a):
+        print(c)
+    else:
+        print(a)
+elif (b < a):
+    print(b)
+else:
+    print(a)
