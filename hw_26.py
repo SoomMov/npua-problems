13a6b2ca5230bd51f089fdf64f9d280129e10487
SoomMov <sum_mov@yahoo.com>
2019-04-12 01:26:00 +0400

    root: init hw_26.

--- hw_26.py
+++ hw_26.py
@@ -2,13 +2,13 @@ a = int(input("input a: "))
 b = int(input("input b: "))
 c = int(input("input c: "))
 
-if (a % 2 == 0):
+if (a % 2 == 0) and (not a == 0):
     print(1)
 
-elif (b % 2 == 0):
+elif (b % 2 == 0) and (not b == 0):
     print(1)
 
-elif (c % 2 == 0):
+elif (c % 2 == 0) and (not c == 0):
     print(1)
 
 else:
