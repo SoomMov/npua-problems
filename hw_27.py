b96ab6d2b6be5376357f4514947385023aa709d5
SoomMov <sum_mov@yahoo.com>
2019-04-11 23:17:38 +0400

    root: init hw27.

--- hw_27.py
+++ hw_27.py
@@ -2,13 +2,13 @@ a = int(input("input a: "))
 b = int(input("input b: "))
 c = int(input("input c: "))
 
-if ():
-    Print(True)
-
-elif ():
+if (c == b + a):
     print(True)
 
-elif ():
+elif (b == c + a):
+    print(True)
+
+elif (a == b + c):
     print(True)
 
 else:
