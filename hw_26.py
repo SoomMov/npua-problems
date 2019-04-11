07284d011f3539eec5fa392d0d042a039a00e4c0
SoomMov <sum_mov@yahoo.com>
2019-04-11 22:39:30 +0400

    root: init hw_26.

---
+++ hw_26.py
@@ -0,0 +1,20 @@
+a = int(input("input a: "))
+b = int(input("input b: "))
+c = int(input("input c: "))
+n = a % 2
+d = b % 2
+f = c % 2
+
+
+if (a == n ) and (n >0):
+    Print(1)
+
+elif (b == d ) and (d >0):
+    print(1)
+
+elif (c == f ) and (c >0):
+    print(1)
+
+else:
+    print(2)
+
---
+++ hw_27.py
@@ -0,0 +1,15 @@
+a = int(input("input a: "))
+b = int(input("input b: "))
+c = int(input("input c: "))
+
+if ():
+    Print(True)
+
+elif ():
+    print(True)
+
+elif ():
+    print(True)
+
+else:
+    print(False)
